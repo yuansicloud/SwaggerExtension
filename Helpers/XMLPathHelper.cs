a66326cbb4c5b8fa343a430de74a833d620562dc
Slarkerino <307221214@qq.com>
2021-05-27 20:28:18 +0800

    添加项目文件。

---
+++ Helpers/XMLPathHelper.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.PlatformAbstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwaggerExtension.Helpers
+{
+    public static class XMLPathHelper
+    {
+        public static string[] XmlCommentsFilePaths
+        {
+            get
+            {
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+
+                string[] files = Directory.GetFiles(basePath, "*.xml", SearchOption.TopDirectoryOnly);
+
+                return files.Select(f => Path.Combine(basePath, f)).ToArray();
+            }
+        }
+    }
+}
